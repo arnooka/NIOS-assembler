a5339bfbc6ad7f5790dcd94506e3b5e77b95da7d
Avinash Nooka <nookavish@gmail.com>
2018-10-21 15:50:56 -0500

    Fixed missing js files issue

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Main Class
+function main() {
+    console.log("Hello World!");
+}
+
+function instructionHandler() {
+    console.log("Handle Instructions...");
+}
+
+function initGUI() {
+    console.log("Initialize GUI");
+}
+
+function verifyFile() {
+    console.log("Verifying File...");
+}
+
+main();
+instructionHandler();
+initGUI();
+verifyFile();
