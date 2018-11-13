29a6a53a015f758c50128380a28504aaeecce14a
Avinash Nooka <nookavish@gmail.com>
2018-11-13 00:09:13 -0600

    Updated file parsing, removed testGui html, formatted server.js file

--- src/Javascript/main.js
+++ src/Javascript/main.js
@@ -20,40 +20,60 @@ function verifyFile() {
         alert('Please upload a ".txt" or ".asm" file');
         return;
     }
-    //console.log('Correct file type uploaded');
     customTxt.innerHTML = asmFile.name;
 
     // Begin reading file
     const reader = new FileReader();
+    reader.readAsText(asmFile);
     let fullFile = "";
     reader.onload = function () {
-        //console.log(reader.result.split('\n'));
         fullFile += reader.result.replace(/,/g, ';').split('\n');
         const lines = fullFile.split(',');
+
         let lineNumber = 1;
         for (let i = 0; i < lines.length; i++) {
             const instruction = parseInstruction(lines[i]);
-            //console.log("i-Val: "+  i + "|" + lines[i]);
-            console.log(instruction);
-            // Verify line in file
+            if (instruction.length > 0) console.log(instruction);
+
+            // Create address for instruction
+            let address = null;
+            if (lineNumber === 1){
+                address = (64).toString(16);
+            } else {
+                address = (64 + lineNumber - 1).toString(16);
+            }
+
+            // Verify instruction
             if (dict.has(instruction[0])) {
-                // Line has a proper instruction
+                // Line has a proper instruction: add to memory
             } else if (instruction.length === 0 || instruction[0] === null || instruction[0].match(/^ *$/) !== null){
-                console.log('Line ' + lineNumber + ' is empty!');
-            } else if (instruction[0].indexOf(':') > -1) {
-                // Add label to label map
-                labels.set(instruction[0], lineNumber);
-                // Ensure next line is considered on the same line as the label
+                //console.log('Line ' + lineNumber + ' is empty!');
                 lineNumber--;
+            } else if (instruction[0].indexOf(':') > -1) {
+                // Line has a label: add to label map
+                console.log('Label Address: 0x' + address + ', Line: ' + lineNumber);
+                labels.set(instruction[0], address);
+
+                // Get instruction if it is on the same line as the label
+                if(instruction.length > 1){
+                    let tempInstruction = [];
+                    for (let j = 1; j < instruction.length; j++) {
+                        tempInstruction.push(instruction[j]);
+                    }
+                    console.log(tempInstruction);
+                    // Add instruction to memory
+                } else {
+                    lineNumber--;
+                }
             } else if (instruction[0].indexOf('.') === 0) {
                 console.log('Line is a heading: ' + instruction[0]);
+                lineNumber--;
             }
             lineNumber++;
         }
         console.log(labels);
     };
 
-    reader.readAsText(asmFile);
     console.log(customTxt.innerHTML);
 }
 
